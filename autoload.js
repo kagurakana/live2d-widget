6bdfb3c461ff9172a648ce0e36ec090805cf10a6
kagurakana <37036280+kagurakana@users.noreply.github.com>
2020-03-25 19:10:00 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/kagurakana/live2d-widget-alter@v0.7.5/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/kagurakana/live2d-widget-alter@0.7.5/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
